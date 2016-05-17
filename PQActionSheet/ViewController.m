4d327d5cd6d86bedc53161ee4b1fc003a6602c42
Follow your heart <392891804@qq.com>
2016-05-17 17:51:34 +0800

    Update ViewController.m
    clean code

--- PQActionSheet/ViewController.m
+++ PQActionSheet/ViewController.m
@@ -23,27 +23,36 @@
 - (IBAction)pressBlockButtonAction:(UIButton *)sender {
     WCActionSheet *actionSheet = [[WCActionSheet alloc] init];
     NSInteger titleIndex = [actionSheet addButtonWithTitle:@"Title"];
+    __weak __typeof(self)weakSelf = self;
     [actionSheet addButtonWithTitle:@"HELLO " actionBlock:^{
         NSLog(@"HELLO");
+        [weakSelf alertTitle:@"ActionBlock" message:@"HELLO"];
     }];
     [actionSheet addButtonWithTitle:@"SOMEONE LIKE YOU" actionBlock:^{
         NSLog(@"SOMEONE LIKE YOU");
+        [weakSelf alertTitle:@"ActionBlock" message:@"SOMEONE LIKE YOU"];
     }];
     
     [actionSheet addButtonWithTitle:@"BEAUTIFUL IN WHITE" actionBlock:^{
         NSLog(@"BEAUTIFUL IN WHITE");
+        [weakSelf alertTitle:@"ActionBlock" message:@"BEAUTIFUL IN WHITE"];
     }];
     
     [actionSheet setButtonTextAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:14],
                                            NSForegroundColorAttributeName:[UIColor blackColor]}
                                 forState:UIControlStateNormal];
-    [actionSheet setButtonTextAttributes:@{NSForegroundColorAttributeName:[UIColor grayColor]}      withIndex:titleIndex
+    [actionSheet setButtonTextAttributes:@{NSForegroundColorAttributeName:[UIColor grayColor]}
+                               withIndex:titleIndex
                                    state:UIControlStateNormal];
     [actionSheet show];
 }
 
 - (IBAction)pressDelegateButtonAction:(UIButton *)sender {
-    WCActionSheet *actionSheet = [[WCActionSheet alloc] initWithDelegate:self cancelButtonTitle:@"Cancel" title:@"DELEGATE TITLE" otherButtonTitles:@"HELLO",@"WORLD",@"HELLO WORLD", nil];
+    
+    WCActionSheet *actionSheet = [[WCActionSheet alloc] initWithDelegate:self
+                                                       cancelButtonTitle:@"Cancel"
+                                                                   title:@"DELEGATE TITLE"
+                                                       otherButtonTitles:@"HELLO",@"WORLD",@"HELLO WORLD", nil];
     [actionSheet show];
 }
 
@@ -51,6 +60,17 @@
 
 - (void)actionSheet:(WCActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
     NSLog(@"BUTTON INDEX = %ld button index of title = %@",(long)buttonIndex,[actionSheet buttonTitleAtIndex:buttonIndex]);
+    [self alertTitle:[NSString stringWithFormat:@"Click index :%d",buttonIndex]
+             message:[actionSheet buttonTitleAtIndex:buttonIndex]];
+}
+
+- (void)alertTitle:(NSString *)title message:(NSString *)message {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
+                                                    message:message
+                                                   delegate:nil
+                                          cancelButtonTitle:nil
+                                          otherButtonTitles:@"ok", nil];
+    [alert show];
 }
 
 @end
